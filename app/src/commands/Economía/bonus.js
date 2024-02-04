dbc28c5e4c4ade9ec739a54832c3bdfdad0d2622
omar11011 <atarama11011@gmail.com>
2024-02-04 17:25:01 -0500

    Creando el comando s!bonus

---
+++ app/src/commands/Economía/bonus.js
@@ -0,0 +1,38 @@
+const Command = require('../../class/Command')
+
+const { axios } = require('../../services')
+const createEmbed = require('../../utils/createEmbed')
+
+module.exports = new Command({
+    name: "bonus",
+    description: "Gana Pokémonedas cada cierto tiempo.",
+    cooldown: 300,
+	async execute(message, props) {
+        let money = 5 + Math.floor(Math.random() * 11)
+        let inc = (await axios.update({
+            url: 'user/balance',
+            props: {
+                user: message.author.id,
+                'balance.money': money,
+            },
+        })).data
+
+        if (inc.modifiedCount < 1) {
+            await axios.create({
+                url: 'user',
+                props: {
+                    user: message.author.id,
+                    balance: { money },
+                }
+            })
+        }
+
+        return createEmbed({
+            message,
+            data: {
+                color: 'green',
+                description: `Has recibido **${money} Pokémonedas** 🤑`,
+            },
+        })
+	},
+})

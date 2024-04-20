d9370da97a5e7cbd81c9873d44ac7524ab98ee96
omar11011 <atarama11011@gmail.com>
2024-04-20 10:16:49 -0500

    Estilizando los mensajes del comando setspawn

--- app/src/commands/Configuración/setspawn.js
+++ app/src/commands/Configuración/setspawn.js
@@ -8,39 +8,39 @@ module.exports = new Command({
     description: "Establece un canal como spawn para atrapar Pokémon salvajes.",
     cooldown: 4,
 	async execute(message, props) {
-        let emoji = message.client.emoji
-        let embed = { color: 'red' }
         let server = (await axios.create({
             url: 'serena/server',
             props: { server: message.guild.id },
         })).data
 
-        if (server.spawn.find(e => e.channel == message.channel.id)) {
-            embed.description = `${emoji("error")} Este canal ya forma parte de los spawn del servidor.`
-        }
-        else {
-            if (server.spawn.length >= 2) embed.description = `${emoji("error")} El servidor ya alcanzó el límite de canales de spawn.`
-            else {
-                embed.color = 'green'
-                embed.description = `${emoji("check")} Has añadido este canal como spawn para el servidor.`
+        let itIsSpawn = server.spawn.find(e => e.channel == message.channel.id)
+        let canBeAdded = server.spawn.length < 2
 
-                server.spawn.push({ channel: message.channel.id })
-                
-                await axios.update({
-                    url: 'serena/server',
-                    props: {
-                        server: message.guild.id,
-                        set: {
-                            spawn: server.spawn,
-                        },
+        if (!itIsSpawn && canBeAdded) {
+            server.spawn.push({ channel: message.channel.id })
+
+            await axios.update({
+                url: 'serena/server',
+                props: {
+                    server: message.guild.id,
+                    set: {
+                        spawn: server.spawn,
                     },
-                })
-            }
+                },
+            })
         }
 
         return createEmbed({
             message,
-            data: embed,
+            data: {
+                color: itIsSpawn || !canBeAdded ? 'red' : 'green',
+                description: 
+                    itIsSpawn
+                    ? `❌ Este canal ya forma parte de los spawn del servidor.`
+                    : canBeAdded
+                        ? `✅ Has añadido este canal como spawn para el servidor.`
+                        : `❌ El servidor ya alcanzó el límite de canales de spawn.`,
+            },
         })
 	},
 })

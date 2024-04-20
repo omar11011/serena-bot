1468283463d760a061694cae90c185a4f0137c34
omar11011 <atarama11011@gmail.com>
2024-04-20 10:02:08 -0500

    Estilizando los mensajes del comando prefix

--- app/src/commands/Configuración/prefix.js
+++ app/src/commands/Configuración/prefix.js
@@ -1,7 +1,10 @@
 const Command = require('../../class/Command')
-const { axios } = require('../../services')
+const createEmbed = require('../../utils/createEmbed')
+
 const megadb = require('megadb')
 const db = new megadb.crearDB('prefix', 'server')
+
+const { axios } = require('../../services')
 
 module.exports = new Command({
     name: 'prefix',
@@ -12,25 +15,31 @@ module.exports = new Command({
 	async execute(message, props) {
         let prefix = props.prefix
         let newPrefix = props.args[0].toLowerCase()
-
-        if (newPrefix.length > 2) return message.reply(`El prefijo no puede tener más de dos caracteres.`)
-
-        if (prefix === process.env.BOT_PREFIX && newPrefix !== process.env.BOT_PREFIX) {
-            await db.establecer(message.guild.id, newPrefix)
-        }
-        else if (prefix !== process.env.BOT_PREFIX) {
-            if (newPrefix === process.env.BOT_PREFIX) await db.eliminar(message.guild.id)
-            else await db.establecer(message.guild.id, newPrefix)
+        let embed = {
+            message,
+            data: { color: 'red', description: `El prefijo no puede tener más de dos caracteres.` },
         }
 
-        await axios.update({
-            url: 'serena/server',
-            props: {
-                server: message.guild.id,
-                set: { 'config.prefix': newPrefix },
-            },
-        })
+        if (newPrefix.length <= 2) {
+            if (prefix === process.env.BOT_PREFIX && newPrefix !== process.env.BOT_PREFIX) {
+                await db.establecer(message.guild.id, newPrefix)
+            }
+            else if (prefix !== process.env.BOT_PREFIX) {
+                if (newPrefix === process.env.BOT_PREFIX) await db.eliminar(message.guild.id)
+                else await db.establecer(message.guild.id, newPrefix)
+            }
+    
+            await axios.update({
+                url: 'serena/server',
+                props: {
+                    server: message.guild.id,
+                    set: { 'config.prefix': newPrefix },
+                },
+            })
 
-        return message.reply(`Mi nuevo prefijo en el servidor es **${newPrefix}**`)
+            embed.data.description = `Mi nuevo prefijo en el servidor es **${newPrefix}**`
+        }
+
+        return createEmbed(embed)
 	},
 })

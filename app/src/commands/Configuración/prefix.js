2edb762babb9a9188d0379f576e8cb0817f610ad
omar11011 <atarama11011@gmail.com>
2024-04-20 09:52:43 -0500

    Modificando el mensaje del comando prefix y su actualización en la BD

--- app/src/commands/Configuración/prefix.js
+++ app/src/commands/Configuración/prefix.js
@@ -1,6 +1,5 @@
 const Command = require('../../class/Command')
-const createEmbed = require('../../utils/createEmbed')
-
+const { axios } = require('../../services')
 const megadb = require('megadb')
 const db = new megadb.crearDB('prefix', 'server')
 
@@ -13,25 +12,25 @@ module.exports = new Command({
 	async execute(message, props) {
         let prefix = props.prefix
         let newPrefix = props.args[0].toLowerCase()
-        let emoji = message.client.emoji
-        let data = {
-            color: 'darkRed',
-            description: `${emoji("error")} El prefijo no puede tener más de dos caracteres.`,
+
+        if (newPrefix.length > 2) return message.reply(`El prefijo no puede tener más de dos caracteres.`)
+
+        if (prefix === process.env.BOT_PREFIX && newPrefix !== process.env.BOT_PREFIX) {
+            await db.establecer(message.guild.id, newPrefix)
+        }
+        else if (prefix !== process.env.BOT_PREFIX) {
+            if (newPrefix === process.env.BOT_PREFIX) await db.eliminar(message.guild.id)
+            else await db.establecer(message.guild.id, newPrefix)
         }
 
-        if (newPrefix.length <= 2) {
-            data.color = 'darkGreen'
-            data.description = `${emoji("check")} Mi nuevo prefijo en el servidor es **${newPrefix}**`
+        await axios.update({
+            url: 'serena/server',
+            props: {
+                server: message.guild.id,
+                set: { 'config.prefix': newPrefix },
+            },
+        })
 
-            if (prefix === process.env.BOT_PREFIX && newPrefix !== process.env.BOT_PREFIX) {
-                await db.establecer(message.guild.id, newPrefix)
-            }
-            else if (prefix !== process.env.BOT_PREFIX) {
-                if (newPrefix === process.env.BOT_PREFIX) await db.eliminar(message.guild.id)
-                else await db.establecer(message.guild.id, newPrefix)
-            }
-        }
-
-        return createEmbed({ message, data })
+        return message.reply(`Mi nuevo prefijo en el servidor es **${newPrefix}**`)
 	},
 })

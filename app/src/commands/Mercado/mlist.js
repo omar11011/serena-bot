2eb954e729f7174e1a9a4722fca0b3eb18342c0f
omar11011 <atarama11011@gmail.com>
2024-04-21 01:13:00 -0500

    Editando el comando mlist

--- app/src/commands/Mercado/mlist.js
+++ app/src/commands/Mercado/mlist.js
@@ -8,36 +8,19 @@ module.exports = new Command({
     alias: ["mlist"],
     description: "Muestra todos los pokémon en venta.",
     cooldown: 6,
-    args: ['option'],
 	async execute(message, props) {
-        props.args = props.args.map(e => e.toLowerCase())
-
         let page = 1
-        let option = props.args[0].toLowerCase()
-        let emoji = message.client.emoji
-        
-        if (!['p', 'i'].includes(option)) return message,reply('Opción inválida.')
-        else {
-            if (option == 'p') option = 'capture'
-            else if (option == 'i') option = 'item'
-            else if (option == 'c') option = 'card'
+        let url = `serena/capture?market=yes&limit=3`
 
-            props.args.shift()
-        }
-        
+        props.args = props.args.map(e => e.toLowerCase())
         props.args.map(e => {
             if (!isNaN(e) && parseInt(e) > 0) page = e
         })
 
-        let url = `serena/${option}?market=yes&limit=3`
-
-        if (option == 'capture') {
-            let shiny = props.args.indexOf('shiny')
-            
-            if (shiny >= 0) {
-                url += `&shiny=yes`
-                props.args.splice(shiny, 1)
-            }
+        let shiny = props.args.indexOf('shiny')
+        if (shiny >= 0) {
+            url += `&shiny=yes`
+            props.args.splice(shiny, 1)
         }
 
         if (props.args.length > 0) url += `&name=${props.args.join(' ')}`
@@ -60,7 +43,7 @@ module.exports = new Command({
                     let NEWDATA = (await axios.get({ url: url + `&page=${page}` })).data.data
                     
                     if (NEWDATA.length > 0) {
-                        m.react(emoji('check'))
+                        m.react('✅')
                         msg.edit({ embeds: [ await sendEmbed(NEWDATA, page, props.prefix) ] })
                     }
                 }
@@ -77,7 +60,7 @@ async function sendEmbed(data, page, prefix) {
         data: {
             color: 'green',
             author: `Mercado Pokémon - Página ${page}`,
-            description: 'A continuación podrás observar todo lo que está en venta. Recuerda que puedes obtener más información usando el comando `' + prefix + 'mshow <option> <ID>`, y compar con el comando `' + prefix +'mbuy <option> <ID>`.',
+            description: 'A continuación podrás observar todo lo que está en venta. Recuerda que puedes obtener más información usando el comando `' + prefix + 'mshow <ID>`, y compar con el comando `' + prefix +'mbuy <ID>`.',
             fields: data.map(e => {
                 let iv = e.stats ? (e.stats.map(f => f.points).reduce((a, b) => a + b, 0) * 100 / (31 * e.stats.length)).toFixed(2) : null
 
